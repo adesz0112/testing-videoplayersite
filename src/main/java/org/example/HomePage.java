08ac1422d9ef1d2d93b5dc4c93b20fe8cf3b0241
adesz0112 <meszaros.adam94@gmail.com>
2025-11-26 14:42:47 +0100

    Created HomePage

--- src/main/java/org/example/HomePage.java
+++ src/main/java/org/example/HomePage.java
@@ -1,5 +1,28 @@
 package org.example;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class HomePage extends BasePage {
+
+    private static final String URL = "https://www.youtube.com";
+    private By searchInput = By.name("search_query");
+    private By searchButton = By.id("search-icon-legacy");
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void openHomePage() {
+        open(URL);
+    }
+
+    public void clickSearchButton() {
+        click(searchButton);
+    }
+
+    public void search(String query) {
+        type(searchInput, query);
+        clickSearchButton();
+    }
 }
